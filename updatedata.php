575f9ea28a4c19e1564cd0a361df29bb1a0e37e3
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-08-31 19:06:11 +0600

    save and update

--- updatedata.php
+++ updatedata.php
@@ -1,15 +1,19 @@
 <?php
 
-
 try {
+    $stu_id = $_POST['sid'];
     $stu_address = $_POST['saddress'];
-    $stu_class = $_POST['class'];
+    $stu_class = $_POST['sclass'];
     $stu_phone = $_POST['sphone'];
     $stu_name = $_POST['sname'];
 
     include 'db.php';
-    $sql = "insert into student(sname, address, sclass, sphone) values('{$stu_name}', '{$stu_address}', '{$stu_class}', '{$stu_phone}')";
+
+    $sql = "update student set sname='{$stu_name}', address='{$stu_address}', sclass='{$stu_class}', sphone='{$stu_phone}' where sid={$stu_id}";
+    // var_dump($sql);
+    // die;
     $result = $conn->query($sql);
+
     header('Location: http://localhost/crud/index.php');
 
     $conn->close();
