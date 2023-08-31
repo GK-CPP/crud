f0bfef2f2e97df1a834361b314bee8514812b208
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-08-31 16:28:12 +0600

    sql conn

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <?php
+include 'db.php';
+
+$sql = "select * from student join studentclass where student.sclass= studentclass.cid";
+$result = $conn->query($sql);
+
+//echo "<pre>";
+$data = $result->fetch_all(MYSQLI_ASSOC);
+//print_r($data[0]['sid']);
+//die;
+
+
+// return as api response josn
+// echo json_encode($data);
+// die;
 include 'header.php';
+
 ?>
+
 <div id="main-content">
     <h2>All Records</h2>
 
-    <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'crud') or die('Connection Failed');
-    $sql = "select * from student join studentclass where student.sclass= studentclass.cid";
-    $result = mysqli_query($conn, $sql) or die('Query Failed.');
-
-    if (mysqli_num_rows($result) > 0) {
-        //     while ($row = mysqli_fetch_assoc($result)) {
-        //         echo $row['sid'] . ' ' . $row['sname'] . ' ' . $row['saddress'] . ' ' . $row['sclass'] . ' ' . $row['sphone'] . '<br>';
-        //     }
-        // }
-
-
+    <?php if (count($data) > 0) {
     ?>
-
         <table cellpadding="7px">
             <thead>
                 <th>Id</th>
@@ -28,25 +32,26 @@ include 'header.php';
                 <th>Action</th>
             </thead>
             <tbody>
-                <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                <?php foreach ($data as $row) { ?>
                     <tr>
                         <td><?= $row['sid']; ?></td>
                         <td><?= $row['sname']; ?></td>
                         <td> <?= $row['address']; ?></td>
-                        <td><?= $row['cname']; ?></td>
+                        <td><?= $row['sclass']; ?></td>
                         <td><?= $row['sphone']; ?></td>
                         <td>
-                            <a href='edit.php'>Edit</a>
+                            <a href='edit.php?id=<?= $row['sid']; ?>'>Edit</a>
                             <a href='delete-inline.php'>Delete</a>
                         </td>
                     </tr>
                 <?php } ?>
             </tbody>
         </table>
-    <?php } else {
+    <?php
+    } else {
         echo '<h2>No Record Found </h2>';
     }
-    mysqli_close($conn);
+    $conn->close();
     ?>
 </div>
 </div>
