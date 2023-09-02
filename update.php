6e01eb459c2fb98df306aa156d3a2b5a36da1a3d
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-02 12:22:49 +0600

    update using ID

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <div id="main-content">
     <h2>Edit Record</h2>
-    <form class="post-form" action="" method="post">
+    <form class="post-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <div class="form-group">
             <label>Id</label>
             <input type="text" name="sid" />
@@ -9,32 +9,60 @@
         <input class="submit" type="submit" name="showbtn" value="Show" />
     </form>
 
-    <form class="post-form" action="updatedata.php" method="post">
-        <div class="form-group">
-            <label for="">Name</label>
-            <input type="hidden" name="sid"  value="" />
-            <input type="text" name="sname" value="" />
-        </div>
-        <div class="form-group">
-            <label>Address</label>
-            <input type="text" name="saddress" value="" />
-        </div>
-        <div class="form-group">
-        <label>Class</label>
-        <select name="sclass">
-            <option value="" selected disabled>Select Class</option>
-            <option value="1">BCA</option>
-            <option value="2">BSC</option>
-            <option value="3">B.TECH</option>
-        </select>
-        </div>
-        <div class="form-group">
-            <label>Phone</label>
-            <input type="text" name="sphone" value="" />
-        </div>
-    <input class="submit" type="submit" value="Update"  />
-    </form>
+
+    <?php if (isset($_POST['showbtn'])) {
+        include 'db.php';
+        $sql = "SELECT * FROM student WHERE sid={$_POST['sid']}";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        if (count($row) > 0) {
+
+    ?>
+            <form class="post-form" action="updatedata.php" method="post">
+                <div class="form-group">
+                    <label for="">Name</label>
+                    <input type="hidden" name="sid" value=" <?= $row['sid']; ?>" />
+                    <input type="text" name="sname" value=" <?= $row['sname']; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Address</label>
+                    <input type="text" name="saddress" value=" <?= $row['address']; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Class</label>
+                    <?php
+                    $sql1 = "select * from studentclass";
+                    $result1 = $conn->query($sql1);
+
+                    $data1 = $result1->fetch_all(MYSQLI_ASSOC);
+
+                    if (count($data1) > 0) { ?>
+                        <select name='sclass'>
+                            <?php foreach ($data1 as $row1) {
+                                if ($row1['cid'] == $row['sclass']) { ?>
+                                    <option value=<?= $row1['cid'] ?> selected>
+                                        <?= $row1['cname'] ?>
+                                    </option>
+                                <?php } else { ?>
+                                    <option value=<?= $row1['cid'] ?>>
+                                        <?= $row1['cname'] ?>
+                                    </option>
+                                <?php } ?>
+                            <?php } ?>
+                        </select>
+                    <?php } ?>
+                </div>
+                <div class="form-group">
+                    <label>Phone</label>
+                    <input type="text" name="sphone" value=" <?= $row['sphone']; ?>" />
+                </div>
+                <input class="submit" type="submit" value="Update" />
+            </form>
+    <?php }
+    }
+    ?>
 </div>
 </div>
 </body>
+
 </html>
